f959d1a43cef01ab15c523d08ec9d91800ac7605
Ivy Xu <xyan21418@gmail.com>
2024-12-12 12:07:56 +0200

    exercise 3.13 done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -21,24 +22,24 @@ app.use(cors())
 app.use(express.static('dist'))
 
 let persons = [
-  { 
+  {
     "id": "1",
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": "2",
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": "3",
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": "4",
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ]
@@ -50,24 +51,32 @@ app.get('/', (req, res) => {
 app.get('/info', (req, res) => {
   const date = new Date()
 
-  res.send(`
-    Phonebook has info for ${persons.length} people
-    <br>${date}`)
+  Person.find({}).then(result => {
+    res.send(`
+      Phonebook has info for ${result.length} people
+      <br>${date}`)
+  })
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(result => {
+    res.json(result)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -86,7 +95,7 @@ app.post('/api/persons', (req, res) => {
   const body = req.body
 
   if (!body.name || !body.number) {
-    return res.status(400).json({ 
+    return res.status(400).json({
       error: 'name or number missing'
     })
   }
